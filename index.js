563de773d98c8dc221bf381149a2bd00734a6032
ddizi <ruroo@naver.com>
2021-12-14 16:49:08 +0900

    소스정리

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ query();
 function query() {
     $.ajax({
         method : "GET",
-        url: "http://54.180.124.191:3000",  //전송
+        //url: "http://54.180.124.191:3000",  //전송
+        url: "http://localhost:3000",  //전송
         dataType:"JSON", 
     })
     .done(function(data) {
@@ -18,16 +19,40 @@ function query() {
 
 $('#button-addon2').click(function() { 
     var addTask = $("#addTask").val();
-    alert(addTask);
+    // alert(addTask);
     $.ajax({
         method : "GET",
-        url: `http://54.180.124.191:3000/add?name=${addTask}`,  //전송
+        // url: `http://54.180.124.191:3000/add?name=${addTask}`,  //전송
+        url: `http://localhost:3000/add?name=${addTask}`,  //전송
         dataType:"JSON", 
     })
     .done(function(data) {
         location.reload();
+    })
+    .always(function(xhr, status) { 
+        alert("요청이 완료되었습니다.");
     });
+    
+;
+    
 });
+
+function getCheckboxValue()  {
+    // 선택된 목록 가져오기
+    const query = 'input[name="animal"]:checked';
+    const selectedEls = 
+        document.querySelectorAll(query);
+    
+    // 선택된 목록에서 value 찾기
+    let result = '';
+    selectedEls.forEach((el) => {
+      result += el.value + ' ';
+    });
+    
+    // 출력
+    document.getElementById('result').innerText
+      = result;
+  }
 
 function initial() {
     
@@ -52,7 +77,15 @@ function loadList(array) {
 // add to do
 function addToDo(name, id, done) {
 
+    let check = '';
+    if(done === "Y") {
+        check = `<input id = "check" class="form-check-input me-1" type="checkbox" value=${id} checked></input>`;
+    } else {
+        check = `<input id = "check" class="form-check-input me-1" type="checkbox" value=${id}></input>`;
+    }
+
     const item = `<li class="list-group-item">
+                    ${check}
                     ${name}
                     </li>
                   `;
